358c8d8086529c86c9d45689ee85f0a7e1efd426
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2025-01-12 16:34:21 +0500

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='w-full h-14 bg-gray-700 mt-14 flex justify-center items-center'>
+        <h5 className="font-[poppins] text-white">Made and Developted with ❤️ by Marjan Ahmed</h5>
+    </div>
+  )
+}
+
+export default Footer
